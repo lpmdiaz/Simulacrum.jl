f145fe1bc32fafbaccc9367a3228be07ae95e639
lpmdiaz <leo-diaz@hotmail.fr>
2021-03-24 15:05:35 +1100

    utils functions & tests

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,29 @@
 # extending Base.:(==) to allow equality check on symbolic variables (here dispatched on Symbolics Num type)
 # Symbolics does extend Base.:(==) but calling `x in [x]` and `x == x` where x is of type Num fails
 Base.:(==)(x::Num, y::Num) = x === y
+
+# returns the most specialised type
+get_lowest_subtype(t1::Type, t2::Type) = t1 <: t2 ? t1 : t2
+get_lowest_subtype(t1::Type, t2::Type, tn...) = get_lowest_subtype(get_lowest_subtype(t1, t2), tn...)
+
+# helper function to unwrap Num types
+get_value(n::Num) = Symbolics.value(n)
+get_value(eq::Equation) = Equation(get_value(eq.lhs), get_value(eq.rhs))
+get_value(eqs::Vector{Equation}) = get_value.(eqs)
+
+# helper that returns the name of a symbolic variable, for a few different types
+get_sym_name(s::Term) = s.f.name
+get_sym_name(s::Sym) = s.name
+get_sym_name(s::Num) = get_sym_name(get_value(s))
+
+# helper function that returns the symbolic variable given as input with the given subscript
+function subscript_var(var::Symbolic, sub::Int; iv = Variable(:t))
+	symname = get_sym_name(var)
+	if var isa Term
+		Variable{Symbolics.FnType{Tuple{Any},Real}}(symname, sub)(iv)
+	elseif var isa Sym
+		Variable(symname, sub)
+	end
+end
+subscript_var(v::Num, sub; iv = Variable(:t)) = Num(subscript_var(get_value(v), sub, iv = iv))
+subscript_vars(vars::Vector{T}, sub; iv = Variable(:t)) where {T<:SymTypes} = [subscript_var(var, sub, iv = iv) for var in vars]
--- test/utils.jl
+++ test/utils.jl
@@ -1,6 +1,50 @@
 using Simulacrum
 using Symbolics
+import SymbolicUtils: Term, Sym
 
-@variables x
-@test x == x
-@test x in [x]
+@test Simulacrum.get_lowest_subtype(Term, Term{Real, Nothing}) == Term{Real, Nothing}
+@test Simulacrum.get_lowest_subtype(Sym{Real, Nothing}, Sym) == Sym{Real, Nothing}
+@test Simulacrum.get_lowest_subtype(Sym{Real, Nothing}, Sym, Sym{Real}) == Sym{Real, Nothing}
+
+@variables t x(t) y
+D = Differential(t)
+z = t + t^2
+diff = D(z)
+eq = Equation(diff, x)
+
+# code below is only useful if this is true
+@test typeof(x) == typeof(diff) == typeof(eq.lhs) == typeof(eq.rhs) == Num
+
+# testing unwrapping of Num via get_value
+conv_eq = Simulacrum.get_value(eq)
+@test eq == conv_eq
+@test typeof(conv_eq.lhs) == typeof(conv_eq.rhs) == Term{Real, Nothing}
+eq = Equation(diff, y)
+conv_eq = Simulacrum.get_value(eq)
+@test typeof(conv_eq.rhs) == Sym{Real, Nothing}
+
+@test Simulacrum.get_sym_name(Variable{Symbolics.FnType}(:x)(t)) == :x # Term
+@test Simulacrum.get_sym_name(Variable(:x)) == :x # Sym
+@test Simulacrum.get_sym_name(Num(Variable(:x))) == :x # Num
+
+# subscript_var: Term, Sym, and Num types
+a = Variable{Symbolics.FnType{Tuple{Any},Real}}(:x)(t)
+b = Variable(:x)
+c = (@variables x(t))[1]
+typeof(a) == Term{Real, Nothing}
+typeof(b) == Sym{Real, Nothing}
+typeof(c) == Num
+@test typeof(Simulacrum.subscript_var(a, 1)) == typeof(a)
+@test typeof(Simulacrum.subscript_var(b, 1)) == typeof(b)
+@test typeof(Simulacrum.subscript_var(c, 1)) == typeof(c)
+
+# subscript_var and subscript_vars
+a = Simulacrum.subscript_vars([Variable(:x)], 1)[1]
+b = Simulacrum.subscript_var(Variable(:x), 1)
+@test isequal(a, b)
+
+# subscript_var: Num type
+@variables x(t) y
+@test typeof(Simulacrum.subscript_var(x, 1)) == typeof(x) == Num
+@test typeof(Simulacrum.get_value(Simulacrum.subscript_var(x, 1))) == typeof(Simulacrum.get_value(x))
+@test typeof(Simulacrum.subscript_var(y, 1)) == typeof(y) == Num
