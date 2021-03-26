6a72b1546ca187d2ba6c1458e46220f431f98d31
lpmdiaz <leo-diaz@hotmail.fr>
2021-03-26 11:13:39 +1100

    replace tabs with spaces

--- src/bridges/modelingtoolkit.jl
+++ src/bridges/modelingtoolkit.jl
@@ -1,12 +1,12 @@
 # convert a Reaction to a ChemicalHyperEdge
 function Base.convert(::Type{ChemicalHyperEdge}, r::ModelingToolkit.Reaction)
-	t_subs = eltype(r.substrates)
-	t_prods = eltype(r.products)
-	t = (isequal(t_subs, t_prods) ? t_subs : Simulacrum.get_lowest_subtype(t_subs, t_prods))
-	ChemicalHyperEdge(SpeciesSet(Vector{t}(r.substrates), r.substoich), SpeciesSet(Vector{t}(r.products), r.prodstoich), r.rate)
+    t_subs = eltype(r.substrates)
+    t_prods = eltype(r.products)
+    t = (isequal(t_subs, t_prods) ? t_subs : Simulacrum.get_lowest_subtype(t_subs, t_prods))
+    ChemicalHyperEdge(SpeciesSet(Vector{t}(r.substrates), r.substoich), SpeciesSet(Vector{t}(r.products), r.prodstoich), r.rate)
 end
 function Base.convert(::Type{ChemicalHyperEdge}, rs::AbstractVector{T}) where {T<:ModelingToolkit.Reaction}
-	convert.(ChemicalHyperEdge, rs)
+    convert.(ChemicalHyperEdge, rs)
 end
 
 # now these functions may be defined via the conversion defined above
@@ -16,15 +16,15 @@ ismultimolecular(r::ModelingToolkit.Reaction) = !(isunimolecular(r))
 # convert a ChemicalHyperEdge to a Reaction
 # note that rate(che)[1] is needed -- not ideal
 function Base.convert(::Type{ModelingToolkit.Reaction}, che::ChemicalHyperEdge)
-	Reaction(rate(che)[1], src(che), tgt(che), src_stoich(che), tgt_stoich(che))
+    Reaction(rate(che)[1], src(che), tgt(che), src_stoich(che), tgt_stoich(che))
 end
 function Base.convert(::Type{ModelingToolkit.Reaction}, ches::AbstractVector{T}) where {T<:ChemicalHyperEdge}
-	convert.(ModelingToolkit.Reaction, ches)
+    convert.(ModelingToolkit.Reaction, ches)
 end
 
 # convert a ReactionSystem to a ChemicalHyperGraph
 function Base.convert(::Type{ChemicalHyperGraph}, rsys::ModelingToolkit.ReactionSystem)
-	ChemicalHyperGraph(convert(ChemicalHyperEdge, equations(rsys)))
+    ChemicalHyperGraph(convert(ChemicalHyperEdge, equations(rsys)))
 end
 
 # return reactions from a chemical hypergraph
@@ -32,12 +32,12 @@ mtk_reactions(chg::ChemicalHyperGraph) = convert(ModelingToolkit.Reaction, hyper
 
 # convert a ChemicalHyperGraph to a ReactionSystem
 function Base.convert(::Type{ModelingToolkit.ReactionSystem}, chg::ChemicalHyperGraph; p = [], iv = Variable(:t))
-	ModelingToolkit.ReactionSystem(mtk_reactions(chg), iv, vertices(chg), p)
+    ModelingToolkit.ReactionSystem(mtk_reactions(chg), iv, vertices(chg), p)
 end
 
 # overload the ChemicalHyperGraph constructor to Reaction and ReactionSystem types
 ChemicalHyperGraph(r::ModelingToolkit.Reaction) = ChemicalHyperGraph(convert(ChemicalHyperEdge, r))
 function ChemicalHyperGraph(rs::AbstractVector{T}) where {T<:ModelingToolkit.Reaction}
-	ChemicalHyperGraph(convert(ChemicalHyperEdge, rs))
+    ChemicalHyperGraph(convert(ChemicalHyperEdge, rs))
 end
 ChemicalHyperGraph(rsys::ModelingToolkit.ReactionSystem) = convert(ChemicalHyperGraph, rsys)
--- src/utils.jl
+++ src/utils.jl
@@ -18,12 +18,12 @@ get_sym_name(s::Num) = get_sym_name(get_value(s))
 
 # helper function that returns the symbolic variable given as input with the given subscript
 function subscript_var(var::Symbolic, sub::Int; iv = Variable(:t))
-	symname = get_sym_name(var)
-	if var isa Term
-		Variable{Symbolics.FnType{Tuple{Any},Real}}(symname, sub)(iv)
-	elseif var isa Sym
-		Variable(symname, sub)
-	end
+    symname = get_sym_name(var)
+    if var isa Term
+        Variable{Symbolics.FnType{Tuple{Any},Real}}(symname, sub)(iv)
+    elseif var isa Sym
+        Variable(symname, sub)
+    end
 end
 subscript_var(v::Num, sub; iv = Variable(:t)) = Num(subscript_var(get_value(v), sub, iv = iv))
 subscript_vars(vars::Vector{T}, sub; iv = Variable(:t)) where {T<:SymTypes} = [subscript_var(var, sub, iv = iv) for var in vars]
@@ -31,24 +31,24 @@ subscript_vars(vars::Vector{T}, sub; iv = Variable(:t)) where {T<:SymTypes} = [s
 # wrap SymbolicUtils variables in chemical hyperedges and hypergraphs into Symbolics.Num
 # useful when converting ModelingToolkit types to chemical hypergraphs and hyperedges and still use the Simulacrum functions that are defined on Num
 function Base.convert(::Type{ChemicalHyperEdge{Num}}, che::ChemicalHyperEdge{T}) where {T<:Symbolic}
-	ChemicalHyperEdge(SpeciesSet(Num.(objects(che.src)), src_stoich(che)), SpeciesSet(Num.(objects(che.tgt)), tgt_stoich(che)), weight(che))
+    ChemicalHyperEdge(SpeciesSet(Num.(objects(che.src)), src_stoich(che)), SpeciesSet(Num.(objects(che.tgt)), tgt_stoich(che)), weight(che))
 end
 function Base.convert(::Type{ChemicalHyperGraph{Num}}, chg::ChemicalHyperGraph{T}) where {T<:Symbolic}
-	ChemicalHyperGraph{Num}(Num.(vertices(chg)), convert.(ChemicalHyperEdge{Num}, hyperedges(chg)))
+    ChemicalHyperGraph{Num}(Num.(vertices(chg)), convert.(ChemicalHyperEdge{Num}, hyperedges(chg)))
 end
 
 # convert directed edge indices to oriented hyperedges (one source, several targets from aggregated edges)
 function edge_idx_to_hyperedge_idx(idx::Vector{Tuple{Int, Int}})
 
-	# group ordinary edge indices according to their source vertex;
-	# edge indices in each group have the same source vertex but different target vertex
-	groups = [filter(v -> first(v) == i, idx) for i in 1:maximum(first.(idx))]
+    # group ordinary edge indices according to their source vertex;
+    # edge indices in each group have the same source vertex but different target vertex
+    groups = [filter(v -> first(v) == i, idx) for i in 1:maximum(first.(idx))]
 
-	# make hyperedge indices by concatenating all targets within each group
-	elist = Vector{Tuple{Vector{Int}, Vector{Int}}}(undef, length(groups))
-	@inbounds for (i, group) in enumerate(groups)
-		elist[i] = ([first(group[1])], last.(group))
-	end
-	elist
+    # make hyperedge indices by concatenating all targets within each group
+    elist = Vector{Tuple{Vector{Int}, Vector{Int}}}(undef, length(groups))
+    @inbounds for (i, group) in enumerate(groups)
+        elist[i] = ([first(group[1])], last.(group))
+    end
+    elist
 
 end
